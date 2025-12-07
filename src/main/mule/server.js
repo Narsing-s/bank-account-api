583870a75d3259193db69d9f0a4e075a188f6f0d
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2025-12-07 09:22:05 +0530

    Initialize Express server with static file serving

---
+++ src/main/mule/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Frontend running at http://localhost:${PORT}`);
+});
