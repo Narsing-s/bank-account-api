ab3f1c05d0623ab48fb8c475cc086d99bd10f964
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-03-06 22:04:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const BASE_URL = "https://bank-account-api-jik9pb.5sc6y6-1.usa-e2.cloudhub.io/api/";
+const BASE_URL = "https://bank-account-api-jik9pb.5sc6y6-1.usa-e2.cloudhub.io/api";
 
 app.post("/createAccount", async (req, res) => {
   try {
-    const response = await fetch(`${BASE_URL}/accounts`, {
+
+    const response = await fetch(`${BASE_URL}/accounts?adharNumber=${req.body.adharNumber}&bankName=${req.body.bankName}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -20,6 +21,7 @@ app.post("/createAccount", async (req, res) => {
     const data = await response.text();
 
     res.status(response.status).send(data);
+
   } catch (error) {
     res.status(500).json({
       error: "Server Error",
@@ -27,6 +29,10 @@ app.post("/createAccount", async (req, res) => {
     });
   }
 });
+
+app.get("/", (req,res)=>{
+  res.send("Bank API Proxy Running 🚀")
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
