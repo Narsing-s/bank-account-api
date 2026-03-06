5543097f1659dce25e7ceab6153911bba4075ff8
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-03-06 22:07:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const BASE_URL = "https://bank-account-api-jik9pb.5sc6y6-1.usa-e2.cloudhub.io/api";
+// ✅ Your Mule API URL
+const BASE_URL = "https://bank-account-api-jik9pb.5sc6y6-1.usa-e2.cloudhub.io";
 
 app.post("/createAccount", async (req, res) => {
   try {
 
-    const response = await fetch(`${BASE_URL}/accounts?adharNumber=${req.body.adharNumber}&bankName=${req.body.bankName}`, {
+    const response = await fetch(`${BASE_URL}/accounts`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -18,21 +20,17 @@ app.post("/createAccount", async (req, res) => {
       body: JSON.stringify(req.body)
     });
 
-    const data = await response.text();
+    const data = await response.json();
 
-    res.status(response.status).send(data);
+    res.json(data);
 
   } catch (error) {
     res.status(500).json({
       error: "Server Error",
-      details: error.message
+      message: error.message
     });
   }
 });
-
-app.get("/", (req,res)=>{
-  res.send("Bank API Proxy Running 🚀")
-})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
