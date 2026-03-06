28dcd7e4fb193a22e49d8210f6057f2a36774684
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-03-06 22:18:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const path = require("path");
 
 const app = express();
+
 app.use(express.json());
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
-const BASE_URL = "https://bank-account-api-jik9pb.5sc6y6-1.usa-e2.cloudhub.io";
+const BASE_URL =
+  "https://bank-account-api-jik9pb.5sc6y6-1.usa-e2.cloudhub.io";
 
 // CREATE ACCOUNT
 app.post("/createAccount", async (req, res) => {
@@ -20,8 +24,8 @@ app.post("/createAccount", async (req, res) => {
     });
 
     const data = await response.text();
-    res.status(response.status).send(data);
 
+    res.status(response.status).send(data);
   } catch (error) {
     res.status(500).json({
       error: "Server Error",
@@ -34,6 +38,7 @@ app.post("/createAccount", async (req, res) => {
 app.get("/accounts/:id", async (req, res) => {
   try {
     const response = await fetch(`${BASE_URL}/accounts/${req.params.id}`);
+
     const data = await response.text();
 
     res.status(response.status).send(data);
@@ -50,6 +55,7 @@ app.delete("/accounts/:id", async (req, res) => {
     });
 
     const data = await response.text();
+
     res.status(response.status).send(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
