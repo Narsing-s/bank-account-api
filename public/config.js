1f8be3b401b4fd7f4dd6d8d3a7ebd64ade359d76
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-03-14 23:03:43 +0530

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,10 @@
+<!-- public/config.js -->
+<script>
+  // Default: use local proxy in web
+  window.AppConfig = {
+    mode: "web", // "web" | "android"
+    WEB_PREFIX: "/api",
+    ANDROID_BASE: "https://bank-account-api-xxxxx.5sc6y6-1.usa-e2.cloudhub.io/api"
+    // NOTE: never put client secrets here
+  };
+</script>
